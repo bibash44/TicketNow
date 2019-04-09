9ec0bb4a036182ea61ead3db01903a97d8cc60cf
bibash44 <bibashkatel4@gmail.com>
2019-04-10 00:31:13 +0545

    added a splash screen

--- app/src/main/java/com/example/ticketnow/MainActivity.java
+++ app/src/main/java/com/example/ticketnow/MainActivity.java
@@ -1,13 +1,15 @@
 package com.example.ticketnow;
 
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
+    ActionBar actionBar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
---
+++ app/src/main/java/com/example/ticketnow/SplashScreen.java
@@ -0,0 +1,25 @@
+package com.example.ticketnow;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.view.KeyEvent;
+
+public class SplashScreen extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash_screen);
+
+        final Intent intent = new Intent(SplashScreen.this, MainActivity.class);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(intent);
+            }
+        },2000);
+    }
+}
