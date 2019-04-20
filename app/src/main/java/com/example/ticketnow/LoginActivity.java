d2ef73fe6e9c7721110e5759f6abeac8f04c0b06
bibash44 <bibashkatel4@gmail.com>
2019-04-20 13:29:32 +0545

    splash screen modified

--- app/src/main/java/com/example/ticketnow/LoginActivity.java
+++ app/src/main/java/com/example/ticketnow/LoginActivity.java
@@ -1,13 +1,29 @@
 package com.example.ticketnow;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
+    TextView signup_llink;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        signup_llink=findViewById(R.id.link_to_signup);
+
+        signup_llink.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v.getId()==R.id.link_to_signup){
+            final Intent signup_intent= new Intent(LoginActivity.this, SignUpActivity.class);
+            startActivity(signup_intent);
+        }
     }
 }
--- app/src/main/java/com/example/ticketnow/SignUpActivity.java
+++ app/src/main/java/com/example/ticketnow/SignUpActivity.java
@@ -1,13 +1,29 @@
 package com.example.ticketnow;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
-public class SignUpActivity extends AppCompatActivity {
-
+public class SignUpActivity extends AppCompatActivity implements View.OnClickListener {
+    TextView login_link;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        login_link=findViewById(R.id.link_to_login);
+        login_link.setOnClickListener(this);
+
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v.getId()==R.id.link_to_login){
+            final Intent login_intent= new Intent(SignUpActivity.this, LoginActivity.class);
+            startActivity(login_intent);
+        }
     }
 }
