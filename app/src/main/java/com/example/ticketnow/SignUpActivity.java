0f07912bd2aa2e94f58aff783cb283baf948b842
bibash44 <bibashkatel4@gmail.com>
2019-04-24 00:21:55 +0545

    forms modified

--- app/src/main/java/com/example/ticketnow/SignUpActivity.java
+++ app/src/main/java/com/example/ticketnow/SignUpActivity.java
@@ -3,12 +3,17 @@ package com.example.ticketnow;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.support.v7.app.ActionBar;
 
 public class SignUpActivity extends AppCompatActivity implements View.OnClickListener {
     TextView login_link;
+
+//    Toolbar signup_toolbar;
+//    ActionBar signup_actionbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,6 +21,12 @@ public class SignUpActivity extends AppCompatActivity implements View.OnClickLis
 
         login_link=findViewById(R.id.link_to_login);
         login_link.setOnClickListener(this);
+
+        /*signup_toolbar=findViewById(R.id.signup_toolbar);
+        setSupportActionBar(signup_toolbar);
+
+        signup_actionbar=getSupportActionBar();
+        signup_actionbar.setTitle("Signup form");*/
     }
 
     @Override
